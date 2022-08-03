e5cb90216dbd19af48191d2e0374fe54fad2b047
zhaoboxiang <zbx@hcttop.com>
2022-08-03 21:32:36 +0800

    refactor: finished drag&drop

--- src/app.ts
+++ src/app.ts
@@ -61,6 +61,18 @@ class ProjectState extends State<Project> {
     );
     this.projects.push(newProject);
 
+    this.updateListeners();
+  }
+
+  moveProject(projectId: string, newStatus: ProjectStatus) {
+    const project = this.projects.find((prj) => prj.id === projectId);
+    if (project && project.status !== newStatus) {
+      project.status = newStatus;
+      this.updateListeners();
+    }
+  }
+
+  private updateListeners() {
     for (const listenerFn of this.listeners) {
       listenerFn(this.projects.slice());
     }
@@ -235,8 +247,13 @@ class ProjectList
     }
   }
 
+  @AutoBind
   dropHandler(event: DragEvent): void {
-    console.log(event.dataTransfer!.getData("text/plain"));
+    const prjId = event.dataTransfer!.getData("text/plain");
+    const newStatus =
+      this.type === "active" ? ProjectStatus.Active : ProjectStatus.Finished;
+
+    projectState.moveProject(prjId, newStatus);
   }
 
   @AutoBind
